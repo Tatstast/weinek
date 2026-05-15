53ba84ed0bb9366ac2712011c2b92722f88300b0
WebArs <turlea@webars.at>
2026-05-15 08:39:11 +0200

    Etappe 1: clean rebuild start (header, hero, familie) - skelette fuer rest

---
+++ script.js
@@ -0,0 +1,16 @@
+(function() {
+    var toggle = document.querySelector('.nav-toggle');
+    var links  = document.querySelector('.nav-links');
+    if (toggle && links) {
+        toggle.addEventListener('click', function() {
+            var open = links.classList.toggle('open');
+            toggle.setAttribute('aria-expanded', open);
+        });
+        links.querySelectorAll('a').forEach(function(a) {
+            a.addEventListener('click', function() {
+                links.classList.remove('open');
+                toggle.setAttribute('aria-expanded', 'false');
+            });
+        });
+    }
+})();
